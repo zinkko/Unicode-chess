506645afeae336704705debc59bc974b7ec7e3ca
Ilari Sinkkonen <sinkkonenilari@gmail.com>
2015-04-07 23:55:27 +0300

    refactoring

---
+++ piece.py
@@ -0,0 +1,39 @@
+#!usr/bin/python
+# -*- coding:utf-8 -*-
+
+
+class Piece():
+    chars = {
+      'pawn':('♙','♟'),
+      'knight':('♘','♞'),
+      'bishop':('♗','♝'),
+      'rook':('♖','♜'),
+      'queen':('♕','♛'),
+      'king':('♔','♚')
+    }
+    def init_checks(self):
+        diag = lambda (x1,y1), (x2,y2): abs(x1-x2) == abs(y1-y2)
+        straight = lambda (x1,y1), (x2,y2): x1 == x2 or y1 == y2
+        both = lambda orig, dest: diag(orig, dest) or straight(orig, dest)
+        self.movement = {
+          'knight': lambda (x1,y1), (x2,y2): (abs(x1-x2),abs(y1-y2)) in [(2,1), (1,2)],
+          'rook': straight,
+          'bishop': diag,
+          'queen': both,  
+          'king': lambda (x1,y1), (x2,y2): abs(x1-x2) < 2 and abs(y1-y2) < 2
+        }
+
+
+    def __init__(self, name, color):
+        '''Piece("pawn",1,(5,7)) is a white pawn at e7'''
+        self.name = name
+        self.sign = self.chars[name][color]
+        self.color = color
+        self.init_checks()
+        if name != 'pawn': # pawns will be handled separately
+            self.check = self.movement[self.name]
+    
+    def __str__(self):
+        return self.sign
+
+
---
+++ ui.py
@@ -0,0 +1,68 @@
+# -*- coding:UTF-8 -*-
+
+from game import Game
+
+class UI():
+    
+    def __init__(self, testmode):
+        self.game = Game()
+        self.testmode = testmode
+
+    def print_board(self):
+        board = self.game.board
+        x = 'abcdefgh'
+        for i in range(7,-1,-1):
+            print (i+1),
+            for j in range(8):
+                nappula = board[j][i]
+                if nappula is None:
+                    if (i+j)%2!=0:
+                        print ' ',
+                    else:
+                        print '⬛',
+                else:
+                    print nappula,
+            print ''
+        print ' ',
+        for i in range(8):
+            print x[i:i+1],
+        print ''
+
+    def user_input(self, cmd):
+        if 'hax' in cmd:
+            if not testmode:
+                return
+            asdf,x,y = cmd.split(' ')
+            extract = lambda word: (ord(word[:1])- ord("a"), int(word[1:]) -1)#hax:P
+            orig = extract(x)
+            dest = extract(y)
+            self.game.hax(orig, dest)
+            return 
+        if 'castle' in cmd: # castle
+            if ' ' not in cmd:
+                return
+            king_side =  cmd.split(' ')[1] == 'king'
+            self.game.castle(king_side)
+            return
+        
+        if ' ' not in cmd:
+            return
+        x,y = cmd.split(" ")
+        spots =  [a+str(b+1) for a in 'abcdefgh' for b in range(8)]
+       
+        if not x in spots or not y in spots:
+            return
+        extract = lambda word: (ord(word[:1])- ord("a"), int(word[1:]) -1)#hax:P
+        orig = extract(x)
+        dest = extract(y)
+        self.game.move(orig, dest)
+
+    def start(self):
+        while True:
+            self.print_board()
+            cmd = raw_input()
+            if cmd=="":
+                break
+            self.user_input(cmd)
+
+
